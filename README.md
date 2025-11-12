a41d6befb3396e63fb2560cfd1d51c64e1017677
Oleg Kryuchikhin <adenos90@gmail.com>
2025-11-12 14:37:51 +0300

    Add alembic for migration and correction config in alembic/env

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ pip install fastapi==0.115.5 pydantic-settings==2.6.1 "uvicorn[standard]==0.32.1
 pip install aiosqlite==0.20.0 
 
 pip install "sqlalchemy[asyncio]==2.0.30" 
+
+pip install alembic==1.13.1 
+alembic init --template async alembic
--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -5,9 +5,6 @@ from app.core.db import Base, CommonMixin
 
 
 class MeetingRoom(CommonMixin, Base):
-    class MeetingRoom(CommonMixin, Base):
     # Имя переговорки должно быть не больше 100 символов,
     # уникальным и непустым.
     name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-
-    
