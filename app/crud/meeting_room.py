cbb709aaf757fb9810a62c1eb57070e779ded054
Oleg Kryuchikhin <adenos90@gmail.com>
2025-11-17 16:38:28 +0300

    Add GET for reed all rooms

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -3,7 +3,6 @@ from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
 
@@ -36,3 +35,9 @@ async def get_room_id_by_name(
     )
     db_room_id = db_room_id.scalars().first()
     return db_room_id
+
+
+async def read_all_rooms_from_db(session: AsyncSession) -> list[MeetingRoom]:
+    meeting_room = await session.execute(select(MeetingRoom))
+    meeting_room = meeting_room.scalars().all()
+    return meeting_room
