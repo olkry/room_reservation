27a359b1bc3e60fdd17dc473fa69b9381c5ce52f
Oleg Kryuchikhin <adenos90@gmail.com>
2025-11-24 16:12:58 +0300

    Add settings for User

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     description: str = 'Для персонала'
     database_url: str | None = None
+    secret: str = 'SECRET'
 
     model_config = SettingsConfigDict(env_file='.env')
 
